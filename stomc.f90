1623374e6585e94ffc29f63f2fbc27b6be040b41
Tom Canich <tjc181@psu.edu>
2018-09-05 16:55:56 -0400

    Syntax and formatting fixes in stomc.f90 -- now compiles

--- stomc.f90
+++ stomc.f90
@@ -1,14 +1,14 @@
-      SUBROUTINE  STOMC
+subroutine  STOMC
   use simsphere_mod
 
 !      INCLUDE 'modvars.h'
 
-*/  Calculates the critical stomatal resistance for the
-*/  critical ground water potential
+!  Calculates the critical stomatal resistance for the
+!  critical ground water potential
 
-      FPSICE = 1 + B1 * PSICE
+  FPSICE = 1 + B1 * PSICE
 
-      RSCRIT = RMIN * FS * FPSICE * FT
+  RSCRIT = RMIN * FS * FPSICE * FT
 
-      RETURN
-      END
+  return
+end
