ce1953b081ecd7611f0c244c8100fe4805bcce0e
Tom Canich <tjc181@psu.edu>
2018-10-08 15:57:16 -0400

    Correct initialization problem in test_water.  Not reinitializing variables between calls to WATER was producing inconsistent test results.

--- tests/test_water.f90
+++ tests/test_water.f90
@@ -18,35 +18,22 @@ program test_water
   ! Check results of f, fsub, wgg, w2g
 
   ! Expected results
-  real, parameter :: f_exp = 2.94117594
-  real, parameter :: fsub_exp = 2.94117641
+  real, parameter :: f_exp = 0.999999821
+  real, parameter :: fsub_exp = 1.0
   real, parameter :: wgg_exp = 0.999999821
   real, parameter :: w2g_exp = 1.0
-  real, parameter :: f_time_exp = 2.94117546
-  real, parameter :: fsub_time_exp = 2.94117641
-  real, parameter :: wgg_time_exp = 0.999999642
+  real, parameter :: f_time_exp = 0.999999821
+  real, parameter :: fsub_time_exp = 1.0
+  real, parameter :: wgg_time_exp = 0.999999821
   real, parameter :: w2g_time_exp = 1.0
-  real, parameter :: f_frveg_exp = 2.94117498
-  real, parameter :: fsub_frveg_exp = 2.94117641
-  real, parameter :: wgg_frveg_exp = 0.999999523
+  real, parameter :: f_frveg_exp = 0.999999881
+  real, parameter :: fsub_frveg_exp = 1.0
+  real, parameter :: wgg_frveg_exp = 0.999999881
   real, parameter :: w2g_frveg_exp = 1.0
 
 
   ! arg1 is time, arg2 is BareEvapFlux
   real :: arg1, arg2
-
-
-  wgg = 1.0
-  w2g = 1.0
-  evap = 0.3
-  frveg = 0.5
-  rnet = 0.5
-  xleg = 0.5
-  xlef = 0.5
-  f = 0.0
-  fsub = 0.0
-  wmax = 0.34
-
 
   n = 1
   ntests = 12
@@ -55,6 +42,7 @@ program test_water
   
 
   ! Case I
+  call water_init
   arg1 = 0.0
   arg2 = 0.5
   call water(arg1,arg2)
@@ -68,6 +56,7 @@ program test_water
   n = n + 1
 
   ! Case II (time /= 0 and rnetf < 0)
+  call water_init
   arg1 = 1.0 ! time
   arg2 = 0.5
   call water(arg1,arg2)
@@ -81,9 +70,10 @@ program test_water
   n = n + 1
 
   ! Case III (time /= 0 and rnetf < 0)
+  call water_init
   arg1 = 1.0 ! time
   arg2 = 0.5
-  frveg = 0.0
+  rnet = 0.0
   call water(arg1,arg2)
   tests(n) = assert(eq(f,f_frveg_exp), 'water frveg == 0 f')
   n = n + 1
@@ -93,13 +83,23 @@ program test_water
   n = n + 1
   tests(n) = assert(eq(w2g,w2g_frveg_exp), 'water frveg == 0 w2g')
   n = n + 1
-  write(*,*) f,f_frveg_exp
-  write(*,*) fsub,fsub_frveg_exp
-  write(*,*) wgg,wgg_frveg_exp
-  write(*,*) w2g,w2g_frveg_exp
 
   test_failed = .false.
   call report_tests(tests,test_failed)
   if (test_failed) stop 1
   
+contains
+  subroutine water_init
+    wgg = 1.0
+    w2g = 1.0
+    evap = 0.3
+    frveg = 0.5
+    rnet = 0.5
+    xleg = 0.5
+    xlef = 0.5
+    f = 0.0
+    fsub = 0.0
+    wmax = 1.0
+    return
+  end subroutine water_init
 end program test_water
