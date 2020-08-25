cabd14786233769e70cbf9fb95ada6da744073d3
Tom Canich <tjc181@psu.edu>
2020-08-25 11:23:50 -0400

    Rework build process to accomodate changes in Windows 10.  The new process is a bit simpler.  This commit includes changes to the build scripts for json-fortran and simsphere.  A future commit will update documentation to reference the new process.

--- scripts/build-json-fortran.sh
+++ scripts/build-json-fortran.sh
@@ -5,5 +5,12 @@
 JSONBUILD=$BUILDROOT/json
 JSONDIST=$DISTROOT/externals/json-fortran
 
-$CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD
+# Test to see if we are working on Windows
+if [ $(uname | grep MINGW) ]
+then
+    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD -G "MinGW Makefiles"
+else
+    # Otherwise, assume a Unix-like system
+    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD 
+fi
 $CMAKE --build $JSONBUILD
--- scripts/build.sh
+++ scripts/build.sh
@@ -17,12 +17,27 @@ export FC CMAKE DISTROOT BUILDROOT
 cd $DISTROOT
 git submodule update --init --recursive
 
-# Build json-fortran, libcompare, simsphere
-scripts/build-json-fortran.sh
-$CMAKE -H$DISTROOT -B$BUILDROOT
-$CMAKE --build $BUILDROOT
+# Check if we are on Windows
+if [ $(uname | grep MINGW) ]
+then
+    export FC=gfortran
+    # Build json-fortran, libcompare, simsphere
+    scripts/build-json-fortran.sh
+    $CMAKE -H$DISTROOT -B$BUILDROOT -G "MinGW Makefiles"
+    $CMAKE --build $BUILDROOT
+    # Put the json-fortran shared library where programs can find it
+    # We can probably fix the simsphere CMake configuration to not have to do this...
+    cp $BUILDROOT/json/libjsonfortran.dll $BUILDROOT/bin
+else
+    # Otherwise assume we're on a Unix
+    $CMAKE -H$DISTROOT -B$BUILDROOT
+    $CMAKE --build $BUILDROOT
+fi
 
 # Test the build
 cd $BUILDROOT
 bin/config 
 ctest
+
+
+
