79be0205c1314387e602ce6a0fe30cd01d2e2026
Tom Canich <tjc181@psu.edu>
2020-10-30 14:25:22 -0400

    Backport changes to build system to build static binaries and libraries.  Working towards pre-compiled Linux and Windows release packages.

---
+++ scripts/build.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+#
+# Build the simsphere program and dependencies
+#
+
+
+# These settings should be ok for Ubuntu 18.04.
+FC=${FC:-gfortran-7}
+CMAKE=${CMAKE:-cmake}
+DISTROOT=${DISTROOT:-$HOME/simsphere}
+BUILDROOT=${BUILDROOT:-$DISTROOT/build}
+CMAKE_BUILD_TYPE="static"
+export FC CMAKE DISTROOT BUILDROOT CMAKE_BUILD_TYPE
+
+
+# Download simsphere dependencies
+cd $DISTROOT
+#git submodule update --init --recursive
+
+# Check if we are on Windows
+if [ $(uname | grep MINGW) ]
+then
+    export FC=gfortran
+    # Build json-fortran, libcompare, simsphere
+    scripts/build-json-fortran.sh
+    scripts/build-libcompare.sh
+    $CMAKE -H$DISTROOT -B$BUILDROOT -G "MinGW Makefiles"
+    $CMAKE --build $BUILDROOT
+    # Put the json-fortran shared library where programs can find it
+    # We can probably fix the simsphere CMake configuration to not have to do this...
+    cp $BUILDROOT/json/libjsonfortran.dll $BUILDROOT/bin
+else
+    # Otherwise assume we're on a Unix
+    # Build json-fortran, libcompare, simsphere
+    scripts/build-json-fortran.sh
+    scripts/build-libcompare.sh
+    $CMAKE -H$DISTROOT -B$BUILDROOT
+    $CMAKE --build $BUILDROOT
+fi
+
+# Test the build
+cd $BUILDROOT
+bin/config 
+ctest
+
+
+
