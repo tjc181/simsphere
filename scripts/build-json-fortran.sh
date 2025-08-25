2f2365ada353d0d3299f0e051bde2f9363edca4e
Tom Canich <tjc181@psu.edu>
2025-08-25 14:56:41 -0400

    Update cmake policy minimum and maximum to comply with newer cmake requirements.  As set now, we'll need to up the minimum in the near future from 2.8 to 3.10 (or later).
    Set the CMAKE_POLICY_VERSION_MINIMUM to 3.5 for libcompare and
    json-fortran to adjust, similarly.  May be new upstream versions which
    make this adjustment already...but this will work for now.

--- scripts/build-json-fortran.sh
+++ scripts/build-json-fortran.sh
@@ -5,12 +5,14 @@
 JSONBUILD=$BUILDROOT/json
 JSONDIST=$SIMHOME/externals/json-fortran
 
+CMAKE_FLAGS="-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+
 # Test to see if we are working on Windows
 if [ $(uname | grep MINGW) ]
 then
-    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD -G "MinGW Makefiles"
+    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD $CMAKE_FLAGS -G "MinGW Makefiles"
 else
     # Otherwise, assume a Unix-like system
-    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD 
+    $CMAKE -D SKIP_DOC_GEN:BOOL=TRUE -H$JSONDIST -B$JSONBUILD $CMAKE_FLAGS
 fi
 $CMAKE --build $JSONBUILD
